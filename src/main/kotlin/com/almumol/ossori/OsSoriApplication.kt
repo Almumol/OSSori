c12a2dc711cd9288059995eefb43a02f98ba9971
hjk0761 <hjk0761@naver.com>
2025-05-01 14:52:16 +0900

    feat: 초기 설정

---
+++ src/main/kotlin/com/almumol/ossori/OsSoriApplication.kt
@@ -0,0 +1,11 @@
+package com.almumol.ossori
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OsSoriApplication
+
+fun main(args: Array<String>) {
+	runApplication<OsSoriApplication>(*args)
+}
---
+++ src/test/kotlin/com/almumol/ossori/OsSoriApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.almumol.ossori
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OsSoriApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
