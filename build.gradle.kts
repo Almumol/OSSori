f5ef170b6c22b47700b212be4228d88780f737e8
Moly <102847513+jminkkk@users.noreply.github.com>
2025-06-23 20:30:55 +0900

    멀티모듈 세팅 (#16)
    * feat: application.yml 설정
    
    * chore: build.gradle 자바 버전 명시
    
    * feat: 엔티티 추가
    
    * feat: 초기 데이터를 위한 data.sql 작성
    
    * style: 프로젝트 가이드 프로퍼티명 수정
    
    * feat: 프로젝트 레포지터리 클래스 구현
    
    * feat: 프로젝트 단건 응답 객체 구현
    
    * feat: Bad Request 예외 클래스 생성 및 핸들러 구현
    
    * feat: Project 서비스 및 조회 로직 구현
    
    * feat: Project 컨트롤러 및 조회 로직 구현
    
    * feat: data.sql의 변수명도 엔티티에 맞게 수정
    
    * chore: 루트 모듈로 변경
    
    * chore: 메인 모듈인 app 루트 모듈로 생성
    
    * chore: 기존 모놀리즈 레포 코드들을 app 모듈로 이동
    
    * chore: infra 모듈 설정
    
    * chore: common 모듈 설정
    
    * chore: batch 모듈 설정
    
    * chore: 의존방향 변경
    
    * chore: common 제거, core 생성
    
    * chore: core 하위 모듈 생성, app 모듈 제거
    
    * chore: 기존 모놀리식 시스템의 코드를 core 하위로 변경
    
    * chore: 전체 모듈 이름에 prefix 추가
    
    * chore: 모듈 간 의존성 추가
    
    ---------
    
    Co-authored-by: slimsha2dy <chy2581@naver.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,63 +1,35 @@
 plugins {
-	kotlin("jvm") version "1.9.25"
-	kotlin("plugin.spring") version "1.9.25"
-	id("org.springframework.boot") version "3.4.5"
-	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "1.9.25"
+    kotlin("jvm") version "1.9.25"
 }
 
-group = "com.almumol"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+    group = "com.almumol"
+    version = "0.0.1-SNAPSHOT"
 
-java {
-	sourceCompatibility = JavaVersion.VERSION_21
-	targetCompatibility = JavaVersion.VERSION_21
-
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    repositories {
+        mavenCentral()
+    }
 }
 
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+subprojects {
+    apply(plugin = "java")
+
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    dependencies {
+        "compileOnly"("org.projectlombok:lombok")
+        "annotationProcessor"("org.projectlombok:lombok")
+        "implementation"("org.springframework.boot:spring-boot-starter-actuator")
+        "testImplementation"("org.springframework.boot:spring-boot-starter-test")
+        "testImplementation"("org.jetbrains.kotlin:kotlin-test-junit5")
+        "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
 
-repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-batch")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.batch:spring-batch-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-	compilerOptions {
-		freeCompilerArgs.addAll("-Xjsr305=strict")
-	}
-}
-
-allOpen {
-	annotation("jakarta.persistence.Entity")
-	annotation("jakarta.persistence.MappedSuperclass")
-	annotation("jakarta.persistence.Embeddable")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
