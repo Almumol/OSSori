74dc3afd2beea4813e84776d3fb5309c81ee360b
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2025-06-23 22:04:36 +0900

    feat: 목록 조회 API 구현

--- module-core/module-domain/src/main/kotlin/project/controller/ProjectController.kt
+++ module-core/module-domain/src/main/kotlin/project/controller/ProjectController.kt
@@ -1,8 +1,11 @@
 package com.almumol.ossori.project.controller
 
+import ProjectsResponse
 import com.almumol.ossori.project.dto.response.ProjectResponse
 import com.almumol.ossori.project.service.ProjectService
 import lombok.RequiredArgsConstructor
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,6 +17,10 @@ import org.springframework.web.bind.annotation.RestController
 class ProjectController(
         private val projectService: ProjectService
 ) {
+
+    @GetMapping("/")
+    fun findAllProjects(@PageableDefault pageable: Pageable): ProjectsResponse =
+        projectService.findAllProjects(pageable)
 
     @GetMapping("/{id}")
     fun findProject(@PathVariable id: Long): ProjectResponse =
--- module-core/module-domain/src/main/kotlin/project/service/ProjectService.kt
+++ module-core/module-domain/src/main/kotlin/project/service/ProjectService.kt
@@ -1,9 +1,11 @@
 package com.almumol.ossori.project.service
 
+import ProjectsResponse
 import com.almumol.ossori.global.exception.BadRequestException
 import com.almumol.ossori.project.dto.response.ProjectResponse
 import com.almumol.ossori.project.repository.ProjectRepository
 import jakarta.transaction.Transactional
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
 @Service
@@ -17,5 +19,11 @@ class ProjectService(
                 .orElseThrow { BadRequestException("존재하지 않는 프로젝트입니다.") }
 
         return ProjectResponse.from(project)
+    }
+
+    fun findAllProjects(pageable: Pageable): ProjectsResponse {
+        val projects = projectRepository.findAll(pageable);
+
+        return ProjectsResponse.from(projects);
     }
 }
