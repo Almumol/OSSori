768fc2b81c991553aa7b996b81ce9db58e092f6b
hjk0761 <hjk0761@naver.com>
2025-07-11 17:31:11 +0900

    refactor: Cors 설정값 application.yml 로 추출

--- module-core/module-domain/src/main/kotlin/global/config/CorsConfig.kt
+++ module-core/module-domain/src/main/kotlin/global/config/CorsConfig.kt
@@ -1,20 +1,24 @@
 package com.almumol.ossori.global.config
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@EnableConfigurationProperties(CorsProperties::class)
 @Configuration
-class CorsConfig {
+class CorsConfig(
+    private val corsProperties: CorsProperties
+) {
 
     @Bean
     fun corsConfigurer(): WebMvcConfigurer {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:3000")
-                    .allowedMethods("*")
+                registry.addMapping(corsProperties.mapping)
+                    .allowedOrigins(*corsProperties.origins.toTypedArray())
+                    .allowedMethods(*corsProperties.methods.toTypedArray())
             }
         }
     }
