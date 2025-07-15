6773db0a1cf273751fc2a47cdf77f127d7556edf
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2025-07-15 22:26:09 +0900

    fix: 실행 모듈 module-api로 이동

---
+++ module-api/src/test/kotlin/almumol/ossori/api/OssoriApplicationTests.kt
@@ -0,0 +1,13 @@
+package almumol.ossori.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(classes = [OssoriApplication::class])
+class OssoriApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
