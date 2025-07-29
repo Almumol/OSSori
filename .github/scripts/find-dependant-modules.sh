464725b2708d6981b0bdd18b0165487891356679
hjk0761 <hjk0761@naver.com>
2025-07-29 16:18:41 +0900

    refactor: jq를 사용하여 공백 제거

--- .github/scripts/find-dependant-modules.sh
+++ .github/scripts/find-dependant-modules.sh
@@ -7,10 +7,19 @@ if ! command -v jq &> /dev/null; then
 fi
 
 output=$(./gradlew findDependentModules -PchangedModules="$1")
+echo "output=${output}"
 candidates=$(echo "$output" | grep "candidates=" | cut -d'=' -f2)
 executables=$(echo "$output" | grep "executables=" | cut -d'=' -f2)
+echo "candidates=$candidates"
+echjo "executables=$executables"
 candidates_json=$(echo "$candidates" | tr ',' '\n' | jq -R . | jq -s .)
 executables_json=$(echo "$executables" | tr ',' '\n' | jq -R . | jq -s .)
+echo "candidates_json=${candidates_json}"
+echo "executables_json=${executables_json}"
+candidates_json=$(echo "$candidates" | tr ',' '\n' | jq -R 'gsub("^\\s+|\\s+$"; "")' | jq -s .)
+executables_json=$(echo "$executables" | tr ',' '\n' | jq -R 'gsub("^\\s+|\\s+$"; "")' | jq -s .)
+echo "candidates_json=${candidates_json}"
+echo "executables_json=${executables_json}"
 
 echo "candidates=\"${candidates_json}\"" >> "$GITHUB_OUTPUT"
 echo "executables=\"${executables_json}\"" >> "$GITHUB_OUTPUT"
