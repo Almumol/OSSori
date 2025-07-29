1c7a36f5ac2cf556f3a0a5ef8bad6ee57b4ccce2
hjk0761 <hjk0761@naver.com>
2025-07-29 16:10:56 +0900

    refactor: CI 스크립트 파싱 오류 수정

--- .github/scripts/find-dependant-modules.sh
+++ .github/scripts/find-dependant-modules.sh
@@ -12,5 +12,5 @@ executables=$(echo "$output" | grep "executables=" | cut -d'=' -f2)
 candidates_json=$(echo "$candidates" | tr ',' '\n' | jq -R . | jq -s .)
 executables_json=$(echo "$executables" | tr ',' '\n' | jq -R . | jq -s .)
 
-echo "candidates=$candidates_json" >> "$GITHUB_OUTPUT"
-echo "executables=$executables_json" >> "$GITHUB_OUTPUT"
+echo "candidates=\"${candidates_json}\"" >> "$GITHUB_OUTPUT"
+echo "executables=\"${executables_json}\"" >> "$GITHUB_OUTPUT"
