4f69c3f288370e079d369592f15bacfde6a86c1d
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2025-08-13 14:34:21 +0900

    refactor: 깃헙 목록 조회 api response에 owner 추가

--- module-core/module-client/src/main/kotlin/almumol/ossori/core/client/dto/response/GithubRepositoryResponse.kt
+++ module-core/module-client/src/main/kotlin/almumol/ossori/core/client/dto/response/GithubRepositoryResponse.kt
@@ -6,5 +6,10 @@ data class GithubRepositoryResponse(
     val openIssuesCount: Long,
     val htmlUrl: String,
     val stargazersCount: Long,
+    val owner: GithubOwnerResponse,
 )
 
+data class GithubOwnerResponse(
+    val login: String,
+    val id: Long,
+)
