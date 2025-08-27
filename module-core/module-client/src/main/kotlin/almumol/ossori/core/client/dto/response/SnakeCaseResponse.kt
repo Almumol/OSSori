ffb8badc953179fbc7694e55f5bae3bd6f2a9df3
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2025-08-27 22:09:50 +0900

    fix: SnakeCaseResponse 추상 클래스 추가

---
+++ module-core/module-client/src/main/kotlin/almumol/ossori/core/client/dto/response/SnakeCaseResponse.kt
@@ -0,0 +1,7 @@
+package almumol.ossori.core.client.dto.response
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+abstract class SnakeCaseResponse
