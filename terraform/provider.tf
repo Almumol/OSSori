897603e5fd49b07ec3fc6b9fdf80ce6550beb6b1
slimsha2dy <chy2581@naver.com>
2025-09-01 22:49:03 +0900

    feat: prod 인스턴스 설정 추가

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -12,6 +12,11 @@ resource "aws_key_pair" "terraform_key" {
   public_key = file("~/.ssh/web_admin.pub")
 }
 
+resource "aws_key_pair" "terraform_key_prod" {
+	key_name = "terraform_key_prod"
+	public_key = file(pathexpand("~/.ssh/ossori_ec2_prod.pub"))
+}
+
 resource "aws_security_group" "terraform_ssh_group" {
   name        = "terraform_ssh_group"
   description = "Allow SSH, HTTP, and HTTPS inbound traffic"
@@ -63,6 +68,56 @@ resource "aws_instance" "terraform_dev_instance" {
   tags = {
      "Name" = "dev"
   }
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+}
+
+resource "aws_security_group" "terraform_ssh_group_prod" {
+  name        = "terraform_ssh_group_prod"
+  description = "Allow SSH, HTTP, and HTTPS inbound traffic (prod)"
+
+  ingress {
+	  description = "SSH"
+	  from_port = 22
+	  to_port = 22
+	  protocol = "tcp"
+	  cidr_blocks = []
+  }
+
+  ingress {
+	  description = "HTTP"
+	  from_port = 80
+	  to_port = 80
+	  protocol = "tcp"
+	  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+	  description = "HTTPS"
+	  from_port = 443
+	  to_port = 443
+	  protocol = "tcp"
+	  cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress  {
+	  from_port = 0
+	  to_port = 0
+	  protocol = "-1"
+	  cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "terraform_prod_instance" {
+  ami 			= "ami-0d5bb3742db8fc264" # Ubuntu Server 24.04 LTS (HVM), SSD Volume Type
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.terraform_key_prod.key_name
+  vpc_security_group_ids = [aws_security_group.terraform_ssh_group_prod.id]
+
+  tags = { Name = "prod" }
 
   root_block_device {
     volume_size = 30
